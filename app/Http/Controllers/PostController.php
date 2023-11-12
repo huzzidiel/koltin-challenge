5f808f6beded463cbba7a155d9039182d2727bf3
Huzzidiel Harrison <huzzidieldev@gmail.com>
2023-11-12 11:53:08 -0600

    updated post controller and model with fillable required

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Post;
+use App\Http\Requests\PostRequest;
+use Illuminate\Support\Facades\Auth;
 
 class PostController extends Controller
 {
-    //
+    public function store(PostRequest $request) {
+        $post = $request->all();
+        $post['author_id'] = Auth::user()->author->id;
+        Post::create($post);
+        return redirect(route('dashboard'));
+    }
 }
