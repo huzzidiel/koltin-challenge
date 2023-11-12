1fac400600f58e9c0f89b7cb3526863ca20a1b15
Huzzidiel Harrison <huzzidieldev@gmail.com>
2023-11-12 15:05:04 -0600

    Added form to create post comments

--- app/Http/Controllers/PostCommentsController.php
+++ app/Http/Controllers/PostCommentsController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\PostComments;
+use App\Http\Requests\PostCommmentsRequest;
 
 class PostCommentsController extends Controller
 {
-    //
+    public function store(PostCommmentsRequest $request)
+    {
+        $postComment = $request->all();
+        PostComments::create($postComment);
+        return redirect(route('post.show', request('post_id')));
+    }
 }
