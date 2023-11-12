1db12158e7aeb37efa6cc6957fb361d6939953c7
Huzzidiel Harrison <huzzidieldev@gmail.com>
2023-11-12 13:25:04 -0600

    Added validations to post request

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/', [PostController::class, 'index']);
 
 Route::middleware(['auth'])->group(function () {
     Route::get('/dashboard', function () {
