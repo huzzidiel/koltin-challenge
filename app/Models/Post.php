fe9f2fad7932c618fc3d14caf1cd2d7d842fb4ac
Huzzidiel Harrison <huzzidieldev@gmail.com>
2023-11-12 13:30:47 -0600

    Added helper to model post to show the date on a custom format

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,7 +2,10 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
@@ -10,13 +13,22 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 class Post extends Model
 {
     use HasFactory;
+
     protected $fillable = ['title', 'content', 'author_id'];
 
-    public function author(): BelongsTo {
+    public function author(): BelongsTo
+    {
         return $this->belongsTo(Author::class);
     }
 
-    public function comments(): HasMany {
+    public function comments(): HasMany
+    {
         return $this->hasMany(PostComment::class);
+    }
+
+    protected function createdAt(): Attribute
+    {
+        return Attribute::make(
+            get: fn($value, $attributes) => Carbon::create($value)->format("Y-m-d"));
     }
 }
